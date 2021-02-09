81652e905da84f204defd55dca10bf093cd96534
はなの <nano_3330@icloud.com>
2021-02-10 01:32:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 $(function(){
   var header_pics = ["images/header1.PNG", "images/header2.jpg", "images/header3.JPG"];
-  var num = -1;
-  
-  function slideshowTimer(){
-    if (num == header_pics.length){
+  var num = 0;
+  setInterval(function(){
+    if (num == header_pics.length - 1){
       num = 0;
     } 
     else {
       num ++;
     }
     document.getElementById("header-pic").src = header_pics[num];
-    setTimeout("slideshowTimer()", 5000);
-  };
-  
-  slideshowTimer();
-  
+  },5000);
 });
