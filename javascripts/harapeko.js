ecbdd79239644aa635ebd6612513bb9a265c553e
はなの <nano_3330@icloud.com>
2021-03-13 09:09:36 +0900

    js

--- javascripts/harapeko.js
+++ javascripts/harapeko.js
@@ -1,6 +1,22 @@
+function buildHtml(value){
+  const html =
+   `<div class="user-area" id="user-area">
+      <div class="user-item">
+        <div class="user-msg">
+          <p>${value}</p>
+        </div>
+      </div>
+    </div>`
+  return html;
+}
 const formElement = document.getElementById('chat-button');
 formElement.addEventListener('click', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
-  console.log('ok');
+  const html = buildHtml(data);
+  const element = document.querySelector('#chat-input');
+  const value = element.value;
+  const parentElement = document.querySelector('#chat-area');
+  parentElement.append(html);
+  console.log(userArea);
 }
